f7369afaaf13debf33b92108b5095af899689090
lqqojin <lqqojin@gmail.com>
2020-08-29 22:27:12 +0900

    [video upload form page] onChange 기능 넣기

--- client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Typography, Button, Form, message, Input, Icon } from "antd";
 import Dropzone from "react-dropzone";
 const { TextArea } = Input;
 const { Title } = Typography;
 
+const privateOptions = [
+    { value: 0, label: 'Private' },
+    { value: 1, label: 'Public' }
+];
+
+const categoryOptions = [
+    { value: 0, label: 'Film & Animation' },
+    { value: 1, label: 'Autos & Vehicles' },
+    { value: 2, label: 'Music' },
+    { value: 3, label: 'Pets & Animals' },
+]
+
 const VideoUploadPage = () => {
+
+    const [videoTitle, setVideoTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [Private, setPrivate] = useState(0);
+    const [category, setCategory] = useState('Film & Animation');
+
+    const onTitleChange = (e) => setVideoTitle(e.currentTarget.value)
+    const onDescriptionChange = (e) => setDescription(e.currentTarget.value)
+    const onPrivateChange = (e) => setPrivate(e.currentTarget.value)
+    const onCategoryChange = (e) => setCategory(e.currentTarget.value)
+
     return (
         <div style={{ maxWidth: '700px', margin: '2rem auto'}}>
             <div style={{ textAlign: 'center', marginBottom: '2rem'}}>
@@ -42,20 +65,34 @@ const VideoUploadPage = () => {
                 <br/>
                 <br/>
                 <label>Title</label>
-                <Input onChange value/>
+                <Input
+                    onChange={onTitleChange}
+                    value={videoTitle}
+                />
                 <br/>
                 <br/>
                 <label>Description</label>
-                <TextArea onChange value />
+                <TextArea
+                    onChange={onDescriptionChange}
+                    value={description}
+                />
                 <br/>
                 <br/>
-                <select onChange>
-                    <option />
+                <select onChange={onPrivateChange}>
+                    {privateOptions.map((item, index) => (
+                        <option key={index} value={item.value}>
+                            {item.label}
+                        </option>
+                    ))}
                 </select>
                 <br/>
                 <br/>
-                <select onChange>
-                    <option />
+                <select onChange={onCategoryChange}>
+                    {categoryOptions.map((item, index) => (
+                        <option key={index} value={item.value}>
+                            {item.label}
+                        </option>
+                    ))}
                 </select>
                 <br/>
                 <br/>
