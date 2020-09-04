2e7abeb59a3e5477aa228d4d820638586508381d
lqqojin <lqqojin@gmail.com>
2020-09-04 17:57:13 +0900

    [비디오 Side 비디오]

--- client/src/components/views/VideoDetailPage/VideoDetailPage.js
+++ client/src/components/views/VideoDetailPage/VideoDetailPage.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import { Row, Col, List, Avatar } from 'antd';
 import Axios from "axios";
+import SideVideo from './Section/SideVideo'
 
 const VideoDetailPage = (props) => {
 	const { videoId }= props.match.params;
@@ -36,20 +37,11 @@ const VideoDetailPage = (props) => {
 					</div>
 				</Col>
 				<Col lg={6} xs={24}>
-					
+					<SideVideo />
 				</Col>
 			</Row>
 		)	
-	} else {
-		return (<div>...Loading</div>)
-	}
-	
-	// if(videoDetail.writer) {
-	//	
-	// } else {
-	// 	Divider
-	// }
-	
+	} else return (<div>...Loading</div>)
 }
 
 export default VideoDetailPage
